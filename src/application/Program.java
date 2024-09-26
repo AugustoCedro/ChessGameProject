d2dec0a391c6653bdaba430d8d12910ed9b1e4bf
AugustoRajao <augustocedro9@gmail.com>
2024-09-26 15:39:44 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.model.entities.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("hELLO WORLD");
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
     }
 }
